3ebb57172bb884874f94a5fe9f0e55342cf705c0
ali-foo <alifouani1111@gmail.com>
2025-12-30 13:16:58 +0200

    Revert SSL to rejectUnauthorized false

--- server/db.js
+++ server/db.js
@@ -6,5 +6,5 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? true : false
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
